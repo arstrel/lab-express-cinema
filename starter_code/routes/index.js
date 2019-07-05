d0f7520e07e5c5cbdd11f9c7ba980248e3e7147e
arstrel <arstrel@gmail.com>
2019-07-05 18:51:28 -0400

    Iteration 3 working. No styles

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const movie    = require('../models/movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req,res,next) => {
+  movie.find()
+    .then((theMovieItemFreshFromDB)=>{
+      res.render('moviesPage', {allTheMovies: theMovieItemFreshFromDB})
+    })
+    .catch((err)=> {
+      next(err)
+    })
+});
+
+
+
 
 module.exports = router;
